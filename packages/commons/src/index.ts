549f3bac85cec464ab9df5ed8928fe5d50cb5cf7
Hugo Dzin <humodz065@gmail.com>
2022-12-12 13:33:41 -0300

    BREAKING CHANGE: change order of logs

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -1,6 +1,6 @@
 import { helloSdk } from '@humodz/hello-sdk';
 import { helloOtherImport } from '@humodz/hello-sdk/other-import';
 
-console.log('hello commons');
-helloSdk();
 helloOtherImport();
+helloSdk();
+console.log('hello commons');
