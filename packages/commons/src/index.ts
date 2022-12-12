336cbb3391e8818818d411ebe694cde110bd03b6
Hugo Dzin <humodz065@gmail.com>
2022-12-12 18:57:46 -0300

    some changes

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -1,6 +1,6 @@
 import { helloSdk } from '@humodz/hello-sdk';
 import { helloOtherImport } from '@humodz/hello-sdk/other-import';
 
+helloOtherImport();
 console.log('HELLO COMMONS!');
 helloSdk();
-helloOtherImport();
