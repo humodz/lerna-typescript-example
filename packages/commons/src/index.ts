d1bffd16399195c49a2f62f1dae66178d50ac12d
Hugo Dzin <humodz065@gmail.com>
2022-12-12 13:31:34 -0300

    BREAKING CHANGE: trying to force major bump

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -4,7 +4,3 @@ import { helloOtherImport } from '@humodz/hello-sdk/other-import';
 console.log('hello commons');
 helloSdk();
 helloOtherImport();
-
-// comment to force changes
-// another comment to force changes
-// another comment to force changes
